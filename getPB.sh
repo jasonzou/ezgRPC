fdac0d02545d2cea27cd3f3c5216fd9e7658e617
jason <jason.zou@gmail.com>
2022-04-06 09:44:30 -0400

    initial version based on https://github.com/adamgordonbell/cloudservices

---
+++ getPB.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+protoc src/api/v1/*.proto --go_out=. --go_opt=paths=source_relative --go-grpc_out=.  --go-grpc_opt=paths=source_relative --proto_path=.
---
+++ install.sh
@@ -0,0 +1,2 @@
+go get google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+go get google.golang.org/protobuf/cmd/protoc-gen-go@latest
